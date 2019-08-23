8f33af67de0327e0cbb5ee514f9bc181722db99d
oleg.v.levitskiy@yandex.ru <oleg.v.levitskiy@yandex.ru>
2019-08-23 13:13:09 +0300

    Buttons, Acitvtty 2 added

--- app/src/main/java/demo/application/androidappmultiactivities/MainActivity.java
+++ app/src/main/java/demo/application/androidappmultiactivities/MainActivity.java
@@ -2,13 +2,43 @@ package demo.application.androidappmultiactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView textView;
+    Button btn1 , btn2, btn3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = (TextView)findViewById(R.id.textView);
+        btn1 = (Button)findViewById(R.id.button1);
+        btn2 = (Button)findViewById(R.id.button1);
+        btn3 = (Button)findViewById(R.id.button3);
+
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Button 1 pressed :: Нажат 1");
+                try {
+                    Intent intent = new Intent(MainActivity.this, Activity2.class);
+                    startActivity(intent);
+                    finish();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+
+
     }
+
+
+
 }
