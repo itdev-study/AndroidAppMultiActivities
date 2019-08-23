29b423612a3b50f7a73149635f282842ae40d3d0
oleg.v.levitskiy@yandex.ru <oleg.v.levitskiy@yandex.ru>
2019-08-23 14:42:54 +0300

    WebView, HTML file reader added

--- app/src/main/java/demo/application/androidappmultiactivities/Activity2.java
+++ app/src/main/java/demo/application/androidappmultiactivities/Activity2.java
@@ -3,12 +3,32 @@ package demo.application.androidappmultiactivities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
 
 public class Activity2 extends AppCompatActivity {
+
+    WebView webcontrol;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_2);
+
+        webcontrol = (WebView)findViewById(R.id.webcontrol);
+        WebSettings ws = webcontrol.getSettings();
+        ws.setJavaScriptEnabled(true);
+        ws.setAllowContentAccess(true);
+        ws.setAppCacheEnabled(true);
+        ws.setDomStorageEnabled(true);
+        ws.setUseWideViewPort(true);
+
+
+        webcontrol.setWebViewClient(new WebViewClient() );
+       // webcontrol.loadUrl("https://www.rbc.ru/");
+        webcontrol.loadUrl("file:///android_asset/html1.html");
+
     }
 }
